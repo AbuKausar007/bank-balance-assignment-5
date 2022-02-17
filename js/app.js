aae79b5681608ced4143e9f2fb11c7f1e8764843
AbuKausar007 <a.25kausar@gmail.com>
2022-02-17 19:54:31 +0600

    added commnet in js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// common function for get input filed
 function inputField(fieldId){
     const fieldBox = document.getElementById(fieldId);
     const inputAmount = parseFloat(fieldBox.value);
@@ -14,34 +15,36 @@ function inputField(fieldId){
 document.getElementById('calc-btn').addEventListener('click', function(){
      
     const incomeAmount = inputField('income-box');
-
     const foodAmount = inputField('food-box');
-
     const rentAmount = inputField('rent-box');
-
     const clothAmount = inputField('cloth-box');
 
+    // calculating tota expense
     const totalExpensesField = document.getElementById('total-expenses');
     const totalExpensesAmount = parseFloat(totalExpensesField.innerText);
     console.log(totalExpensesAmount);
     const totalExpensesIs = foodAmount + rentAmount + clothAmount;
     totalExpensesField.innerText = totalExpensesIs;
 
+    //get and update balance after expense
     const balanceExpenseField = document.getElementById('balance');
     const balanceExpenseAmount = parseFloat(balanceExpenseField.innerText);
     const totalBalanceAfterExpense = incomeAmount - totalExpensesIs;
     balanceExpenseField.innerText = totalBalanceAfterExpense;
 
+    // calculating percentage of saving money
     document.getElementById('save-btn').addEventListener('click',function(){
     const percentField = document.getElementById('save-box');
     const percentToGet = parseFloat(percentField.value);
     const savingAmount = (percentToGet/100) * incomeAmount;
     console.log(savingAmount);
 
+    // get and update saving money
     const savingField = document.getElementById('saving-amount');
     const savingMoneyIs = parseFloat(savingField.innerText);
     savingField.innerText = savingAmount;
 
+    // get and update remainging balance
     const remainingField = document.getElementById('remainging-balance');
     const remaingingFieldValue = parseFloat(remainingField.innerText);
     remainingField.innerText = totalBalanceAfterExpense - savingAmount;
